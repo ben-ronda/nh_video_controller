cae5b05a1ca197d619a34fe8fc7836f649ce0faa
Ben Ronda <bronda95@comcast.net>
2017-01-04 15:39:53 -0800

    initial commit. Begin working on index.php content.

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>TLC South Control Interface</title>
+  </head>
+  <body>
+    <h3>System Controls</h3>
+    <div id="projector_control">
+      <h4>Projector</h4>
+      <div id="projector_toggle" class="power">
+        <p>power button here</p>
+      </div>
+    </div>
+
+    <div id="Sound_control">
+      <h4>Projector</h4>
+      <div id="sound_toggle" class="power">
+        <p>power button here</p>
+      </div>
+    </div>
+
+    <h3>Input Selector</h3>
+    <a href="#">DVD</a>
+    <a href="#">Front HDMI</a>
+    <a href="#">Back HDMI</a>
+  </body>
+</html>
