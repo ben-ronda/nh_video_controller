92b142b1d067ef0643d697fd2d195a6047eb7dce
Ben Ronda <bronda95@comcast.net>
2017-01-04 16:09:17 -0800

    add bootstrap and icons

--- index.php
+++ index.php
@@ -3,20 +3,27 @@
   <head>
     <meta charset="utf-8">
     <title>TLC South Control Interface</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/styles.css">
   </head>
   <body>
     <h3>System Controls</h3>
+    
     <div id="projector_control">
       <h4>Projector</h4>
       <div id="projector_toggle" class="power">
-        <p>power button here</p>
+        <button type="button" name="button" class="btn btn-default">
+          <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
+        </button>
       </div>
     </div>
 
     <div id="Sound_control">
       <h4>Projector</h4>
       <div id="sound_toggle" class="power">
-        <p>power button here</p>
+        <button type="button" name="button" class="btn btn-default">
+          <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
+        </button>
       </div>
     </div>
 
@@ -24,5 +31,8 @@
     <a href="#">DVD</a>
     <a href="#">Front HDMI</a>
     <a href="#">Back HDMI</a>
+
+    <h4>Start Roku</h4>
+    <a href="#"></a>
   </body>
 </html>
