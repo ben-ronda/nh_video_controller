35166422a16ac6ba5ea300078c7bbc9244393ffa
Ben Ronda <bronda95@comcast.net>
2017-01-04 16:10:29 -0800

    change the second header fo system copntrols to be 'sound'

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   </head>
   <body>
     <h3>System Controls</h3>
-    
+
     <div id="projector_control">
       <h4>Projector</h4>
       <div id="projector_toggle" class="power">
@@ -19,7 +19,7 @@
     </div>
 
     <div id="Sound_control">
-      <h4>Projector</h4>
+      <h4>Sound</h4>
       <div id="sound_toggle" class="power">
         <button type="button" name="button" class="btn btn-default">
           <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
